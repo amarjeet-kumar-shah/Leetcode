c6bcf25b5f79e2411324ca2f0c17797b530494f6
Amarjeet Kumar Shah <161136422+amarjeet-kumar-shah@users.noreply.github.com>
2025-02-06 22:10:56 +0530

    Time: 4 ms (46.61%) | Memory: 8.8 MB (91.31%) - LeetSync

--- 873-guess-the-word/guess-the-word.cpp
+++ 873-guess-the-word/guess-the-word.cpp
@@ -8,27 +8,44 @@
  */
 class Solution {
 public:
-    int solve(string s1, string s2) {
-        int ans = 0;
-        for (int i = 0; i < s1.size(); i++) {
-            if (s1[i] == s2[i])
-                ans++;
+    // Helper function to calculate how many characters match between two strings
+    // This will be used to compare the guessed word with the current word
+    int numMatches(const string& s1, const string& s2) {
+        int matches = 0;
+        for (int index = 0; index < s1.length(); index++) {
+            if (s1[index] == s2[index]) {
+                matches++;  // Increment count for each matching character
+            }
         }
-        return ans;
+        return matches;
     }
 
+    // Main function to guess the secret word by interacting with the 'Master' API
     void findSecretWord(vector<string>& words, Master& master) {
-        unordered_set<string> st(words.begin(), words.end());
+        // Create a set of all possible words (initial prospects)
+        unordered_set<string> prospects(words.begin(), words.end());
 
-        while (!st.empty()) {
-            string first = *st.begin();
-            int guessed = master.guess(first);
+        // Continue guessing until we run out of prospects
+        while (!prospects.empty()) {
+            // Select the first word from the set as the current guess
+            string frontWord = *prospects.begin();
 
-            for (auto it = st.begin(); it!= st.end();) {
-                if(solve(*it,first) != guessed) it = st.erase(it);
-                else it++;
+            // Use the Master's API to get the number of matching characters with the secret word
+            int numMatchesSecret = master.guess(frontWord);
+
+            // Iterate over the set of prospective words
+            for (auto it = prospects.begin(); it != prospects.end();) {
+                // If the number of matches between the prospective word and the current guess 
+                // is not the same as the matches with the secret word, erase it from the set
+                if (numMatches(*it, frontWord) != numMatchesSecret) {
+                    it = prospects.erase(it);  // Safe removal of elements during iteration
+                } else {
+                    ++it;  // Move to the next word in the set
+                }
             }
-            st.erase(first);
+
+            // Remove the current guess from the prospects set as it has been guessed
+            prospects.erase(frontWord);
         }
     }
 };
