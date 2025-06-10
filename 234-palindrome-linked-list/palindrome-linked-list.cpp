bcb9c64b4fdceecb75592b110e04964e6bf64329
Amarjeet K. Shah <161136422+amarjeet-kumar-shah@users.noreply.github.com>
2025-06-10 11:03:10 +0530

    Time: 2 ms (66.48%) | Memory: 131.9 MB (17.91%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -6,7 +6,7 @@ public:
             listVals.push_back(head->val);
             head = head->next;
         }
-        
+    
         int left = 0, right = listVals.size() - 1;
         while (left < right && listVals[left] == listVals[right]) {
             left++;
