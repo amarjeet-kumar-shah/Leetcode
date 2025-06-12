173d73f3faac83e14fdabbdd8a10ef8b4493ab0e
Amarjeet K. Shah <161136422+amarjeet-kumar-shah@users.noreply.github.com>
2025-06-12 09:37:19 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (74.21%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(!root) return 0;
+        int l = 0, r = 0;
+        for(TreeNode* tree = root; tree; tree = tree -> left) ++l;
        
         return 1 + countNodes(root -> left) + countNodes(root -> right);
     }
