d193a462624f467fd3904633b379ca7ad1469406
Amarjeet K. Shah <161136422+amarjeet-kumar-shah@users.noreply.github.com>
2025-10-17 16:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (47.92%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,10 +3,15 @@ public:
     int hammingWeight(int n) {
         int count =0;
         while(n!=0){
-            if(n&1){
+            if(n &1 ==1){
                 count++;
+                n= n>>1;
             }
-            n= n>>1;
+            else{
+                 n =  n>>1;
+            }
+          
+            
         }
         return count;
     }
